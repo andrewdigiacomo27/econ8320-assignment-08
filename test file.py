0db633c062220aed467cd34305608e0e57534a01
andrewdigiacomo27 <andrewdigiacomo27@gmail.com>
2025-10-24 14:10:06 -0500

    attempt 2

--- test file.py
+++ test file.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import numpy as np
 
 data = {'colA': [10, 20, 30], 'colB': [40, 50, 60]}
 df = pd.DataFrame(data)
 
 df['new_column_of_ones'] = 1
+z = df.values
+n, k = z.shape #n is rows, k is columns, this finds the degrees of freedom (n-k)
+#can't have more columns than rows
