edd6a6e114871a9f0b33a396a3da6aff5186839f
andrewdigiacomo27 <andrewdigiacomo27@gmail.com>
2025-10-27 12:21:22 -0500

    attempt 4

--- test file.py
+++ test file.py
@@ -1,10 +1,67 @@
 import pandas as pd
 import numpy as np
+from scipy.stat import t
 
-data = {'colA': [10, 20, 30], 'colB': [40, 50, 60]}
-df = pd.DataFrame(data)
+class RegressionModel(object):
+    def __init__(self, x, y, create_intercept=True, regression_type="ols"):
+        self.x = pd.DataFrame(x)
+        self.y = pd.DataFrame(y)
+        self.create_intercept = create_intercept
+        self.regression_type = regression_type
+    def add_intercept(self):
+        if self.create_intercept:
+            if "intercept" not in self.x.columns:
+                self.x["intercept"] = 1
+    def ols_regression(self):
+        if self.regression_type == "ols":                                #key is variable
+            X = np.array(self.x)
+            Y = np.array(self.y)
+            n, k = X.shape                                               #df parameters
+            df = n - k
+            beta_coef = np.linalg.inv(X_t * X) @ X.T @ Y                 #coefficient
+            s_2 = ((Y.T * Y) - (Y.T * X * np.linalg.inv(X.T * X) * X.T * Y)) / df #unbiased variance matrix
+            cov_B = s_2 * np.linalg.inv(X.T * X)                         #covariance of b
+            variance = np.diag(cov_B)                                    #variance
+            standardError = np.sqrt(variance)                            #sqrt of variance
+            t_stat = beta_coef / standardError                           #t stat
+            p_val = t.sf(t_stat, df)                                     #p value
 
-df['new_column_of_ones'] = 1
-z = df.values
-n, k = z.shape #n is rows, k is columns, this finds the degrees of freedom (n-k)
-#can't have more columns than rows
+            for num, val in enumerate(self.x.columns):
+                self.results[val] = {
+                    "coefficient": float(beta_coef[num]),
+                    "standard_error": float(standardError[num]),
+                    "t_stat": float(t_stat[num]),
+                    "p_value": float(p_val[num]),
+                }
+
+            return self.results
+    def summary(self):
+        if not self.results:
+            self.ols_regression()
+
+        var_name = []
+        coef_value = []
+        standardError = []
+        t_stat = []
+        p_value = []
+
+
+        table_summary = pd.DataFrame({
+            "Variable name": var_name,
+            "coefficient value": coef_value,
+            "standard error": standardError,
+            "t-statistics": t_stat,
+            "p-value": p_value
+            })
+
+        return table_summary
+
+
+data = pd.read_csv("tests/files/assignment8Data.csv")
+x = data[['sex','age','educ','white']]
+y = data['incwage']
+reg = RegressionModel(x, y, create_intercept=True)
+
+w = reg.ols_regression()
+print(reg.summary())
+
